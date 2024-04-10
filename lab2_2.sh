34015ca01ea0ccd43581d9962d6ca02b67657751
Tetromiko <deberger.studio@gmail.com>
2024-04-10 22:28:45 +0300

    Initial commit

--- lab2_2.sh
+++ lab2_2.sh
@@ -1,16 +1,20 @@
-users={"user1" "user2" "user3"} 
+users=("user1" "user2" "user3")
 
-for user in "$users[@]}"
+for user in "${users[@]}"
 do
-	if [ -d "/home/$user" ]; then
-		echo "Catalogue for user $user already exist"
-	else
-		mkdir "/home/$user"
-		adduser "$user"
-		password=$(openssl rand -base64 12)
-		echo "$user:$password" | chpasswd
-		echo "User password $user: $password" > "/home/$user/password.txt"
-		ssh-keygen -t rsa -b 2048 -f "/home/$user/.ssh/$user_key" -q -N ""
-		echo "User $user created succesfully"
-	fi
+    if [ -d "/home/$user" ]; then
+        echo "Каталог для користувача $user вже існує"
+    else
+        mkdir "/home/$user"
+        
+        adduser "$user"
+        
+        password=$(openssl rand -base64 12)
+        echo "$user:$password" | chpasswd
+        echo "Пароль користувача $user: $password" > "/home/$user/password.txt"
+        
+        ssh-keygen -t rsa -b 2048 -f "/home/$user/.ssh/$user_key" -q -N ""
+        
+        echo "Користувач $user успішно створений"
+    fi
 done
